d7576929bc4bed7a938edc5d71bbbc5e61d5456a
Huazhong Ji <hzji210@gmail.com>
2025-08-02 13:56:11 +0800

    Update plugin.py

--- verl_npu/plugin.py
+++ verl_npu/plugin.py
@@ -3,7 +3,7 @@ from verl_npu.workers.rollout.vllm_rollout.vllm_rollout_spmd import vLLMRolloutP
 from verl_npu.workers.sharding_manager.megatron_vllm import MegatronVLLMSshardingManagerPatch
 from verl_npu.workers.megatron_workers import ActorRolloutRefWorkerPatch
 
-def ver_npu_plugin():
+def apply_npu_plugin():
     # Please make sure to apply the patches 
     # in the correct order so that they can 
     # work properly.
