872fd0595e06544587cd0da271f0af024b42848f
hzji210@gmail.com <hzji210@gmail.com>
2025-08-04 18:33:18 +0800

    update

--- verl_npu/plugin.py
+++ verl_npu/plugin.py
@@ -1,13 +1,8 @@
-from verl_npu.utils.megatron_utils import MegatronUtilsPatch
 from verl_npu.workers.rollout.vllm_rollout.vllm_rollout_spmd import vLLMRolloutPatch
-from verl_npu.workers.sharding_manager.megatron_vllm import MegatronVLLMSshardingManagerPatch
-from verl_npu.workers.megatron_workers import ActorRolloutRefWorkerPatch
+
 
 def apply_npu_plugin():
     # Please make sure to apply the patches 
     # in the correct order so that they can 
     # work properly.
-    MegatronUtilsPatch.apply_patch()
     vLLMRolloutPatch.apply_patch()
-    MegatronVLLMSshardingManagerPatch.apply_patch()
-    ActorRolloutRefWorkerPatch.apply_patch()
