00198bb54b5c6aea7f38e8ec565a245a8393f993
hzji210@gmail.com <hzji210@gmail.com>
2025-08-04 16:37:52 +0800

    update

--- verl_npu/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
+++ verl_npu/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
@@ -9,6 +9,7 @@ from omegaconf import DictConfig, OmegaConf
 from vllm import LLM, SamplingParams
 from vllm.distributed import parallel_state as vllm_ps
 from verl.utils.device import get_device_name, get_torch_device
+from verl.workers.rollout.base import BaseRollout
 from verl.workers.rollout.vllm_rollout.vllm_rollout_spmd import vLLMRollout
 
 from verl_npu.patch_util import NPUPatchHelper
@@ -26,7 +27,7 @@ class vLLMRolloutPatch(NPUPatchHelper[vLLMRollout]):
             model_hf_config: the huggingface config to initiallize the generating model in vllm
             **kwargs: train_tp, for Megatron Backend to initialize hybrid engine (zero redundancy) process group
         """
-        super().__init__()
+        super(BaseRollout, self).__init__()
         self.config = config
 
         tensor_parallel_size = self.config.get("tensor_model_parallel_size", 1)
@@ -106,7 +107,7 @@ class vLLMRolloutPatch(NPUPatchHelper[vLLMRollout]):
         # patch this for npu
         enable_infer_ep = False
         if hasattr(config, "dp_model_parallel_size") and config.dp_model_parallel_size > 1:
-            self._init_dp_envs(config)
+            self._init_dp_env(config)
             enable_infer_ep = True
 
         self.inference_engine = LLM(
@@ -131,18 +132,11 @@ class vLLMRolloutPatch(NPUPatchHelper[vLLMRollout]):
             **lora_kwargs,
             **engine_kwargs,
         )
-        self.model = self.inference_engine.llm_engine.model_executor.driver_worker.worker.model_runner.get_model()
-        self.model_buffers_cpu = {}
-        self.model_buffers = None
-        self.worker = self.inference_engine.llm_engine.model_executor.driver_worker.worker
-        for name, param in self.model.named_parameters():
-            self.model_buffers_cpu[name] = torch.empty_like(param, device="cpu")
 
         # Offload vllm model to reduce peak memory usage
         if config.free_cache_engine:
-            self.free_cache_engine()
-            self.offload_model_weights()
-
+            self.inference_engine.sleep(level=1)
+    
         kwargs = dict(
             n=1,
             logprobs=0,  # can be set to 0 and let actor to recompute
@@ -190,64 +184,3 @@ class vLLMRolloutPatch(NPUPatchHelper[vLLMRollout]):
         envs.VLLM_DP_MASTER_PORT = int(os.environ["VLLM_DP_MASTER_PORT"])
 
         print(f"[VLLM] using TP={tp_size}, DP={dp_size}", flush=True)
-
-    def onload_model_weights(self):
-        self.model_buffers = {}
-        for name, param in self.model.named_parameters():
-            self.model_buffers[name] = torch.empty_like(param, device=get_device_name())
-            param.data = self.model_buffers[name]
-
-    def offload_model_weights(self):
-        for name, params in self.model.named_parameters():
-            params.data = self.model_buffers_cpu[name]
-
-        self.model_buffers = None
-        gc.collect()
-        get_torch_device().empty_cache()
-
-    def init_cache_engine(self):
-        if os.environ["VLLM_USE_V1"] == "1":
-            if not self.worker.model_runner.kv_caches:
-                self.inference_engine.llm_engine.engine_core.engine_core.model_executor.initialize_from_config(
-                    self.inference_engine.llm_engine.engine_core.engine_core.kv_cache_configs)
-                self.inference_engine.llm_engine.reset_prefix_cache()
-        else:
-            if self.inference_engine.llm_engine.model_executor.driver_worker.worker.cache_engine is None:
-                self.inference_engine.llm_engine.model_executor.driver_worker.worker._init_cache_engine()
-
-    def free_cache_engine(self):
-        if os.environ["VLLM_USE_V1"] == "1":
-            ctx = self.worker.model_runner.vllm_config.compilation_config.static_forward_context
-        else:
-            ctx = self.inference_engine.llm_engine.model_executor.driver_worker.worker.compilation_config.static_forward_context
-        from vllm.attention import AttentionType
-
-        layer_need_kv_cache = []
-        for layer_name in ctx:
-            if hasattr(ctx[layer_name], 'attn_type') and ctx[layer_name].attn_type in (AttentionType.DECODER, AttentionType.ENCODER_DECODER):
-                layer_need_kv_cache.append(layer_name)
-
-        pipeline_parallel_size = self.inference_engine.llm_engine.vllm_config.parallel_config.pipeline_parallel_size
-        for layer_name in layer_need_kv_cache:
-            kv_cache = []
-            for _ in range(pipeline_parallel_size):
-                kv_cache.append(torch.tensor([]))
-            ctx[layer_name].kv_cache = kv_cache
-        if os.environ["VLLM_USE_V1"] == "1":
-            self.worker.model_runner.kv_caches = []
-        else:
-            self.inference_engine.llm_engine.model_executor.driver_worker.worker.cache_engine = None
-            self.inference_engine.llm_engine.model_executor.driver_worker.worker.gpu_cache = None
-        if hasattr(self.model, 'model') and hasattr(self.model.model.layers[0].self_attn, "attn"):
-            for i in range(self.model.model.start_layer, self.model.model.end_layer):
-                attn_impl = self.model.model.layers[i].self_attn.attn.impl
-                if hasattr(attn_impl, "key_cache"):
-                    attn_impl.key_cache = None
-                    attn_impl.value_cache = None
-        # 多模态kv cache
-        elif hasattr(self.model, 'language_model') and hasattr(self.model.language_model.model.layers[0].self_attn, "attn"):
-            for i in range(self.model.language_model.model.start_layer, self.model.language_model.model.end_layer):
-                attn_impl = self.model.language_model.model.layers[i].self_attn.attn.impl
-                if hasattr(attn_impl, "key_cache"):
-                    attn_impl.key_cache = None
-                    attn_impl.value_cache = None
