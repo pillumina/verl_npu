dae67bd1cb38caa366ae6fe26d4c3e791bb6e537
Pillumina <derios1230@gmail.com>
2025-08-12 14:43:02 +0800

    [fix] remove update_actor

--- verl_npu/workers/megatron_workers.py
+++ verl_npu/workers/megatron_workers.py
@@ -1,80 +1,13 @@
-import psutil
-import warnings
-from codetiming import Timer
-from megatron.core import mpu
-from verl import DataProto
-from verl.single_controller.base.decorator import make_nd_compute_dataproto_dispatch_fn, register
 from verl.workers.megatron_workers import ActorRolloutRefWorker, logger
-from verl.utils.device import get_device_name, get_torch_device
+from verl.utils.device import get_device_name
 from verl.utils.fs import copy_to_local
 from verl.utils.profiler import (
-    DistProfiler, 
-    GPUMemoryLogger,
     log_gpu_memory_usage,
 )
-from verl.utils.megatron_utils import (
-    load_megatron_model_to_gpu,
-    load_megatron_optimizer,
-    offload_megatron_model_to_cpu,
-    offload_megatron_optimizer,
-)
-from verl.utils.memory_utils import aggressive_empty_cache
 from verl_npu.patch_util import NPUPatchHelper
 
 
 class ActorRolloutRefWorkerPatch(NPUPatchHelper[ActorRolloutRefWorker]):
-
-    @register(dispatch_mode=make_nd_compute_dataproto_dispatch_fn(mesh_name="actor"))
-    @GPUMemoryLogger(role="update_actor", logger=logger)
-    @DistProfiler.annotate(color="red")
-    def update_actor(self, data: DataProto):
-        assert self._is_actor
-        if self._is_offload_param:
-            load_megatron_model_to_gpu(self.actor_module)
-            log_gpu_memory_usage("After load actor params and grad during update_actor", logger=logger)
-        if self._is_offload_optimizer:
-            load_megatron_optimizer(self.actor_optimizer)
-            log_gpu_memory_usage("After load actor optimizer during update_actor", logger=logger)
-        # reload hccl groups
-        if hasattr(mpu, "reload_process_groups"):
-            mpu.reload_process_groups()
-
-        data.batch = data.batch.to(get_device_name())
-
-        micro_batch_size = self.config.actor.ppo_micro_batch_size_per_gpu
-        data.meta_info["micro_batch_size"] = micro_batch_size
-        dataloader = self.actor.make_minibatch_iterator(data=data)
-        with Timer(name="update_policy", logger=None) as timer:
-            metrics = self.actor.update_policy(dataloader=dataloader)
-        delta_time = timer.last
-        global_num_tokens = data.meta_info["global_token_num"]
-        estimated_flops, promised_flops = self.flops_counter.estimate_flops(global_num_tokens, delta_time)
-        metrics["perf/mfu/actor"] = estimated_flops * self.config.actor.ppo_epochs / promised_flops / self.world_size
-        metrics["perf/max_memory_allocated_gb"] = get_torch_device().max_memory_allocated() / (1024**3)
-        metrics["perf/max_memory_reserved_gb"] = get_torch_device().max_memory_reserved() / (1024**3)
-        metrics["perf/cpu_memory_used_gb"] = psutil.virtual_memory().used / (1024**3)
-        from verl.utils.megatron.optimizer import get_megatron_last_lr
-
-        metrics["actor/lr"] = get_megatron_last_lr(self.actor_optimizer)
-        self.actor_optimizer_scheduler.step(1)
-
-        # TODO: here, we should return all metrics
-        output = DataProto(meta_info={"metrics": metrics})
-        output = output.to("cpu")
-
-        if self._is_offload_param:
-            offload_megatron_model_to_cpu(self.actor_module)
-            log_gpu_memory_usage("After offload actor params and grad during update_actor", logger=logger)
-        if self._is_offload_optimizer:
-            offload_megatron_optimizer(self.actor_optimizer)
-            log_gpu_memory_usage("After offload actor optimizer during update_actor", logger=logger)
-        # offload hccl groups
-        if hasattr(mpu, "destroy_process_groups"):
-            mpu.destroy_process_groups()
-
-        aggressive_empty_cache(force_sync=True)
-        return output
-
 
     def _build_rollout(self, trust_remote_code=False):
             from torch.distributed.device_mesh import init_device_mesh
